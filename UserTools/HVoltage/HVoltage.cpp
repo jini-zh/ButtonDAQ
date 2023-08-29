a20027ca00e065c41ca682c65372760aba7e108c
Evgenii Zhemchugov <evgenii.zhemchugov@warwick.ac.uk>
2023-08-29 17:54:07 +0100

    HVoltage: implement slow control support

--- UserTools/HVoltage/HVoltage.cpp
+++ UserTools/HVoltage/HVoltage.cpp
@@ -35,17 +35,58 @@ void HVoltage::connect() {
 };
 
 void HVoltage::configure() {
+  auto& ui = m_data->SC_vars;
+
   std::stringstream ss;
-  for (int i = 0; i < boards.size(); ++i)
+  for (unsigned i = 0; i < boards.size(); ++i)
     for (int channel = 0; channel < 6; ++channel) {
       float voltage = 0;
       ss.str({});
       ss << "hv_" << i << "_ch_" << channel;
-      if (m_variables.Get(ss.str(), voltage)) {
-        boards[i].set_voltage(channel, voltage);
-        boards[i].set_power(channel, true);
-      } else
-        boards[i].set_power(channel, false);
+      bool power = m_variables.Get(ss.str(), voltage) && voltage > 0;
+      boards[i].set_voltage(channel, voltage);
+      boards[i].set_power(channel, power);
+
+      auto set_voltage = [vars = &ui, board = &boards[i], channel](std::string name)
+        -> std::string
+      {
+        auto value = vars->GetValue<float>(name);
+        board->set_voltage(channel, value);
+        return std::move(name) + ' ' + std::to_string(value);
+      };
+
+      ss.str({});
+      ss << "hv_" << i << "_channel_" << channel << "_voltage";
+      auto name = ss.str();
+      if (!ui.Add(name, VARIABLE, set_voltage))
+        throw std::runtime_error(
+            "failed to add slow control element `" + name + '\''
+        );
+      auto element = ui[name];
+      element->SetMin(0);
+      element->SetMax(boards[i].voltage_max(channel));
+      element->SetStep(0.1);
+      element->SetValue(voltage);
+
+      auto set_power = [vars = &ui, board = &boards[i], channel](std::string name)
+        -> std::string
+      {
+        auto value = vars->GetValue<std::string>(name);
+        board->set_power(channel, value == "on");
+        return std::move(name) + ' ' + value;
+      };
+
+      ss.str({});
+      ss << "hv_" << i << "_channel_" << channel << "_power";
+      name = ss.str();
+      if (!ui.Add(name, OPTIONS, set_power))
+        throw std::runtime_error(
+            "failed to add slow control element `" + name + '\''
+        );
+      element = ui[name];
+      element->AddOption("on");
+      element->AddOption("off");
+      element->SetValue(power ? "on" : "off");
     };
 };
 
@@ -57,6 +98,8 @@ bool HVoltage::Initialise(std::string configfile, DataModel& data) {
   m_log  = m_data->Log;
 
   if (!m_variables.Get("verbose", m_verbose)) m_verbose = 1;
+
+  m_data->SC_vars.InitThreadedReceiver(m_data->context, 5555);
 
   try {
     connect();
